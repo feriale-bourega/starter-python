468e056d9561bae101ad54b7dd0c867a34e68432
jade <jade@debian>
2022-10-17 16:33:11 +0200

    script job 04

---
+++ job04.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 
+nombre =int(input('Entrez un nombre : '))
+print ('Bonjour,', nombre)
+i = 0
+while i <= nombre:
+    print("i a pour valeur",i)
+    i = i + 1
+    
+print("Fin") 
+
