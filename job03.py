bdfe3f4811c5e68c63ad95ecaef259d9a0909320
jade <jade@debian>
2022-10-17 15:55:30 +0200

    Script job 03

---
+++ job03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3 
+print("Bonjour,quel âge as-tu?")
+age =int(input("Entrez votre âge : "))
+print("Bonjour,", age)
+if age < 18:
+          print("Tu es mineur")
+
+if age >= 18:
+          print("TU es majeur")
