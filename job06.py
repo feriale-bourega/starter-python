04c68cb78b617d2c3a200490ac31741f992029ce
jade <jade@debian>
2022-10-18 10:50:52 +0200

    script job06

---
+++ job06.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3 
+print("<")
+var1 = (input("Entrez votre texte : "))
+while var1 == "Bonjour":
+          print("Bonjour à toi")
+          if var1 == "Au revoir":
+            break
