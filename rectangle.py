a084b2dffb2fbfa2e7d52024262f28372e1eaf1c
jade <jade@debian>
2022-10-18 16:05:50 +0200

    script job08

---
+++ rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+hauteur = int(input("hauteur:"))
+largeur = int(input("largeur:"))
+side = "|"
+separator = "-"
+for i in range(hauteur):
+    if i == 0 or i == hauteur-1:
+          separator = "-"
+    else:
+          separator = " "
+    print( side + separator*largeur + side )
