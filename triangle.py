0b3659aa548476f8ed1845d7bab78dc9b133f53e
jade <jade@debian>
2022-10-18 16:35:30 +0200

    script job09

---
+++ triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 
+left = "/"
+right = "\\"
+base = "__"
+userinput = int (input("hauteur:"))
+userinput = 10
+for i in range(userinput):
+    print((userinput-i)*" " + left + ((i*2)*" ") + right)
+    if i == userinput-1:
+       print(left + base*userinput + right)
